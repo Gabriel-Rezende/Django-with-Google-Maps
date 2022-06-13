8ac3be88a9f0b8ae1b1d058502d453ed98a12553
Gabriel Pires <gabriel.pires@intelliway.com.br>
2022-06-12 21:19:04 -0300

    DB configs

--- join/join/settings.py
+++ join/join/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^*q141q(v9ys8b*_-ju)10+cih2%%)_&v1je2i76jr_d=)h3pv'
+SECRET_KEY = 'palsdpasldapsldpasdlpsl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'join.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Join Ex10',
-        'USER': 'postgres',
-        'PASSWORD': 'gpcr19042002',
+        'NAME': 'YOUR_DB_NAME_HERE',
+        'USER': 'YOUR_USER_HERE',
+        'PASSWORD': 'YOUR_PASSWORD_HERE',
         'PORT': '5432',
     }
 }
