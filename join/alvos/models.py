5adf96575f1662c612101cdc07a415855792948d
Gabriel Pires <gabriel.pires@intelliway.com.br>
2022-06-12 22:03:13 -0300

    Lat Long max and min values

--- join/alvos/models.py
+++ join/alvos/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Alvo(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=150)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(
+            max_digits=9,
+            decimal_places=6,
+            validators=[
+                MaxValueValidator(90),
+                MinValueValidator(-90)
+            ])
+    longitude = models.DecimalField(
+            max_digits=9,
+            decimal_places=6,
+            validators=[
+            MaxValueValidator(180),
+            MinValueValidator(-180)
+        ])
     data_expiracao = models.DateField()
 
     class Meta:
